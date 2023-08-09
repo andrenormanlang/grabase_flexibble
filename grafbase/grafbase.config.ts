2a1b03e4940cefb4b369291fd7f7777075348f32
andrenormanlang <andrenormanlang@gmail.com>
2023-08-09 22:17:51 +0200

    fixing graphql

--- grafbase/grafbase.config.ts
+++ grafbase/grafbase.config.ts
@@ -1,13 +1,14 @@
-import { g, config, auth } from '@grafbase/sdk';
+import { g, auth, config } from '@grafbase/sdk'
 
 // @ts-ignore
 const User = g.model('User', {
-  name: g.string().length({ min: 2, max: 100 }),
+  name: g.string().length({ min:2, max:20}),
   email: g.string().unique(),
   avatarUrl: g.url(),
-  description: g.string().length({ min: 2, max: 1000 }).optional(),
+  description: g.string().optional(),
   githubUrl: g.url().optional(),
-  linkedinUrl: g.url().optional(), 
+  linkedinUrl: g.url().optional(),
+  // @ts-ignore
   projects: g.relation(() => Project).list().optional(),
 }).auth((rules) => {
   rules.public().read()
@@ -16,10 +17,10 @@ const User = g.model('User', {
 // @ts-ignore
 const Project = g.model('Project', {
   title: g.string().length({ min: 3 }),
-  description: g.string(), 
+  description: g.string(),
   image: g.url(),
-  liveSiteUrl: g.url(), 
-  githubUrl: g.url(), 
+  liveSiteUrl: g.url(),
+  githubUrl: g.url(),
   category: g.string().search(),
   createdBy: g.relation(() => User),
 }).auth((rules) => {
